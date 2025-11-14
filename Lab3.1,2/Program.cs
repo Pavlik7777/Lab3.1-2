6ee931937dc63a2d825c7e97729a8ead9e6f091e
Pavlik7777 <Pasha20080620@gmail.com>
2025-11-14 15:25:44 +0200

    Program.cs

--- Lab3.1,2/Program.cs
+++ Lab3.1,2/Program.cs
@@ -16,15 +16,20 @@
 //Оператор цикла for
 //Базовый уровень. Вариант 22
 
-Console.WriteLine("Введите x:");
-double x = double.Parse(Console.ReadLine());
-double r = double.Parse(Console.ReadLine());
-if (0 < x && x < Math.PI)
-    r = Math.Sin(x);
-else if (-Math.PI <= x && x < 0)
-    r = Math.Pow(x, 3);
-else if (x < -2 * Math.PI || x > 2 * Math.PI)
-    r = Math.Pow(x, 2);
-else
-    r = 0;
-Console.WriteLine($"{r}");
+Console.WriteLine("Введите n:");
+int n = int.Parse(Console.ReadLine());
+for (int i = 1; i <= n; i++)
+{
+    Console.WriteLine("Введите x:");
+    double x = double.Parse(Console.ReadLine());
+    double r = double.Parse(Console.ReadLine());
+    if (0 < x && x < Math.PI)
+        r = Math.Sin(x);
+    else if (-Math.PI <= x && x < 0)
+        r = Math.Pow(x, 3);
+    else if (x < -2 * Math.PI || x > 2 * Math.PI)
+        r = Math.Pow(x, 2);
+    else
+        r = 0;
+    Console.WriteLine($"{r}");
+}
